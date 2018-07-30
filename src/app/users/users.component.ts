22a5e8fcd011739b575deabf0b8f180e9c417be8
vic lotana <viclotana@gmail.com>
2018-07-30 08:49:45 +0100

    clearing

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -16,10 +16,7 @@ import { Dev } from '../models/dev';
   //         animate('550ms ease-out',
   //       style({ opacity: 1, transform: 'translateY(0px)'})))
   //       ], {optional: true}),
-  //       query(':leave', animate('50ms', style({ opacity: 0 })),{optional: true})
-  //     ])
-  //   ]) 
-  // ]
+ 
 })
 export class UsersComponent implements OnInit {
   devs: Dev[];
