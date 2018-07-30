f8935b7ca668c62a48c908dd14b7710fecbf3da8
vic lotana <viclotana@gmail.com>
2018-07-30 09:06:48 +0100

    adding content to events service

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from
+'angularfire2/firestore';
+import { Dev } from '../models/Dev';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
+   devCollection: AngularFirestoreCollection<Dev>;
+   devs: Observable<Dev[]>;
 
-  constructor() { }
+  constructor(public lax: AngularFirestore) { 
+    this.devs = this.lax.collection('events').valueChanges();
+  }
+
+  getDevelopers(){
+    return this.devs;
+  }
 }
