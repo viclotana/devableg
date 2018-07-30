b32d35c416ef46532ceb35a8576032e20b3a0fe7
vic lotana <viclotana@gmail.com>
2018-07-30 08:50:18 +0100

    clearing

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -14,7 +14,7 @@ import { Dev } from '../models/dev';
   //         style({ opacity: 0, transform: 'translateY(-35px)' }),
   //         stagger('50ms', 
   //         animate('550ms ease-out',
-  //       style({ opacity: 1, transform: 'translateY(0px)'})))
+ 
 
  
 })
