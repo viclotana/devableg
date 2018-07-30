a509f538e95ee59575d9db3edde45dd568ee2a43
vic lotana <viclotana@gmail.com>
2018-07-30 08:34:24 +0100

    update2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
-import 
 import { PostsComponent } from './posts/posts.component';
 
 const routes: Routes = [
