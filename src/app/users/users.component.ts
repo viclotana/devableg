9e0d6a59cdb3c8ad7e074c581927022df5735d86
vic lotana <viclotana@gmail.com>
2018-07-30 08:49:05 +0100

    clearing

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DevService } from '.././services/dev.service';
 import { Dev } from '../models/dev';
-// import { trigger,transition,query,style,stagger,animate,keyframes }
-// from '@angular/animations'
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
