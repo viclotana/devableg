7bbbb7e6d6493c418f55752fec55d1e41f5d36d5
vic lotana <viclotana@gmail.com>
2018-07-31 08:54:08 +0100

    built dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCQ5yXXjrDOp1sYlmR4TM1wMUW7le4P780",
+    authDomain: "nollywood1-358bf.firebaseapp.com",
+    databaseURL: "https://nollywood1-358bf.firebaseio.com",
+    projectId: "nollywood1-358bf",
+    storageBucket: "nollywood1-358bf.appspot.com",
+    messagingSenderId: "1036356969034"
+  }
 };
